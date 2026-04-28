f41590269a2695d99759b870d6c939c31fe181a7
sloweroons <bertalanabel2@gmail.com>
2026-04-28 21:23:07 +0200

    added Flask #2

--- app/app.py
+++ app/app.py
@@ -1,14 +1,18 @@
+import subprocess
+import pytesseract
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/health")
-def health():
-    return "OK"
-
-@app.route("/")
-def hello_world():
-    return "Hello, World!!"
+@app.route("/test-ocr")
+def test_ocr():
+    try:
+        py_ver = pytesseract.get_tesseract_version()
+        sys_ver = subprocess.check_output(["tesseract", "--version"]).decode("utf-8")
+        
+        return f"<h1>OCR Status: OK</h1><p>Pytesseract version: {py_ver}</p><pre>{sys_ver}</pre>"
+    except Exception as e:
+        return f"<h1>OCR Error</h1><p>Pytesseract missing{str(e)}</p>"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
