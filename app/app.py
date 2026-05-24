fae184166dfdccab72a01fd55e342a430fb6b726
sloweroons <bertalanabel2@gmail.com>
2026-05-24 21:03:18 +0200

    Updated webpage #4

--- app/app.py
+++ app/app.py
@@ -24,7 +24,7 @@ def root():
             <input type="submit" value="Upload">
         </form>
        
-        <h2>OCR Status: OK</h2><p>Pytesseract version: {py_ver}</p><pre>{sys_ver}</pre>
+        <p>OCR Status: OK</p><br><p>Pytesseract version: {py_ver}</p><pre>{sys_ver}</pre>
     "   
 
 @app.route('/upload', methods=['POST'])
