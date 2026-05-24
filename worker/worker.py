c4a27a68114eebca3e8f4590e8b26d9bdd9ca27d
sloweroons <bertalanabel2@gmail.com>
2026-05-24 21:39:34 +0200

    Added worker subprocess

---
+++ worker/worker.py
@@ -0,0 +1,13 @@
+import redis
+import time
+
+print("Operator Notification Worker elindult, varakozas az uzenetekre...", flush=True)
+
+# Kapcsolódás a Kubernetes-en belüli Redis szervizhez
+r = redis.Redis(host='manual-redis-service-service', port=6379, decode_responses=True)
+pubsub = r.pubsub()
+pubsub.subscribe('operator_notifications')
+
+for message in pubsub.listen():
+    if message['type'] == 'message':
+        print(f"[UZEMELTETOI ERTESITES]: {message['data']}", flush=True)
