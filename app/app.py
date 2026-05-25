ef07b88bfb4ca66bf6c4cd8f3b39c332b437294d
sloweroons <bertalanabel2@gmail.com>
2026-05-25 16:38:29 +0200

    asdasd

--- app/app.py
+++ app/app.py
@@ -5,6 +5,7 @@ from flask import Flask, request
 import pytesseract
 from datetime import datetime
 from PIL import Image, ImageDraw
+import boto3
 
 app = Flask(__name__)
 
@@ -14,6 +15,19 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 build_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 redis_client = redis.Redis(host='manual-redis-service', port=6379, decode_responses=True, password='adminpass')
+
+s3_client = boto3.client(
+    's3',
+    endpoint_url='http://manual-minio-service:9000',
+    aws_access_key_id='admin',
+    aws_secret_access_key='adminpass'
+)
+BUCKET_NAME = 'ocr-images'
+
+try:
+    s3_client.create_bucket(Bucket=BUCKET_NAME)
+except Exception:
+    pass
 
 # ROOT ENDPOINT
 @app.route("/")
@@ -61,19 +75,30 @@ def upload():
                 draw.rectangle([x, y, x + w, y + h], outline="red", width=3)
 
         # TESTING
-        output_path = os.path.join(UPLOAD_FOLDER, "proc_" + file.filename)
+        output_path = os.path.join(UPLOAD_FOLDER, "_"+file.filename)
         img.save(output_path)
         
         detected_text = " ".join([word.strip() for word in d['text'] if word.strip()])
-        description_and_text = f"{description};<b>{detected_text}</b>"
+        description_and_text = f"{description};{detected_text}"
         redis_client.set(file.filename, description_and_text)
         redis_client.publish('operator_notifications', description_and_text)
+
+        s3_client.upload_file(
+            Filename=output_path,
+            Bucket=BUCKET_NAME,
+            Key=f"proc_{file.filename}",
+            ExtraArgs={
+                "Metadata": {
+                    "description-and-text": description_and_text.encode('utf-8').decode('latin1')
+                }
+            }
+        )
         
         return f"""
             <p>Description: {description_and_text}.<p>
             <p>Image location for testing: {output_path}</p>
             <form action="/" method="get">
-                <input type="submit" value="Return">
+                <input type="submit" value="Back to Home">
             </form>
         """
     except Exception as e:
