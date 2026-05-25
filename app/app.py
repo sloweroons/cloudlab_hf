4c77b61054e8d5de77f71fb7a2722041ad90f13b
sloweroons <bertalanabel2@gmail.com>
2026-05-26 00:12:15 +0200

    MinIO #6

--- app/app.py
+++ app/app.py
@@ -125,7 +125,9 @@ def database():
                     ExpiresIn=3600
                 )
                 external_url = raw_url.replace('http://manual-minio-service:9000', 'http://localhost:30009')
-                image = f'<img src="{external_url}" alt="Processed Image" style="max-width: 150px; height: auto;">'            except Exception:
+                image = f'<img src="{external_url}" alt="Processed Image" style="max-width: 150px; height: auto;">'  
+            except Exception:
+                image = "No processed image available"
 
             table_rows += f"""
                 <tr>
