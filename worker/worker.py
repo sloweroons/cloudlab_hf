1c60f71718394c0475a6712e7661cffd0c8552d6
sloweroons <bertalanabel2@gmail.com>
2026-05-25 22:33:53 +0200

    Broker fixes #7

--- worker/worker.py
+++ worker/worker.py
@@ -44,4 +44,4 @@ pubsub.subscribe('operator_notifications')
 for message in pubsub.listen():
     if message['type'] == 'message':
         current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        print(f"-- # {current_time} - received new OCR data: {message['data']}", flush=True)
+        print(f"-- # {current_time} - New data: {message['data']}", flush=True)
