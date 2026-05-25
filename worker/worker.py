960c225cdbdd6ddee5fb99f7b2c61a2eafb89d0c
sloweroons <bertalanabel2@gmail.com>
2026-05-25 22:17:52 +0200

    a

--- worker/worker.py
+++ worker/worker.py
@@ -1,36 +1,47 @@
 import redis
 import time
+from datetime import datetime
 
-print("-> worker thread initializing", flush=True)
+current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+print(f"-> worker thread initializing - {current_time}", flush=True)
 
 r = redis.Redis(host='manual-redis-service', port=6379, decode_responses=True, password='adminpass')
 
-print("-> Attempting connection with in-memory Redis database", flush=True)
+current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+print(f"-> Attempting connection with in-memory Redis database - {current_time}", flush=True)
 while True:
     try:
         if r.ping():
             break
     except redis.exceptions.ConnectionError:
-        print("-- # Redis offline, waiting...", flush=True)
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"-- # Redis offline, waiting... - {current_time}", flush=True)
         time.sleep(2)
-print("-- # Redis online", flush=True)
-print("-> Fetching Redis in-memory database", flush=True)
+
+current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+print(f"-- # Redis online - {current_time}", flush=True)
+print(f"-> Fetching Redis in-memory database - {current_time}", flush=True)
 try:
     all_keys = r.keys('*')
     if not all_keys:
-        print("-- # No previous uploads found.")
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"-- # No previous uploads found - {current_time}", flush=True)
     else:
         for filename in all_keys:
             description_and_text = r.get(filename)
+            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             print(f"-- #{filename};{description_and_text}")
 except Exception as e:
-    print(f"-- # Error fetching data: {e}")
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"-- # Error fetching data: {e} - {current_time}", flush=True)
 
-print("-> enabling notifications")
+current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+print(f"-> enabling notifications - {current_time}")
 
 pubsub = r.pubsub()
 pubsub.subscribe('operator_notifications')
 
 for message in pubsub.listen():
-    if message['type'] == 'message':
-        print(f"-- # received new OCR data: {message['data']}")
+    if message['type'] == 'message'
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S"):
+        print(f"-- # received new OCR data: {message['data']} - {current_time}", flush=True)
